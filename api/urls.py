7c63efbb1ba31e37a16e0470a5970f1a65395835
Jonah Tuchow <tuchowj@carleton.edu>
2017-06-04 14:46:06 -0500

    rest_framework tutorial part 2

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.snippet_list),
     url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- djangoTemplate/settings.py
+++ djangoTemplate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'home.apps.HomeConfig',
     'api.apps.ApiConfig',
     'django.contrib.auth.apps.AuthConfig',
