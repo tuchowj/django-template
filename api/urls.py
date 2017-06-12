04ebf88919dd39fa68b801eb23f40d71990e75e7
Jonah Tuchow <tuchowj@carleton.edu>
2017-06-12 15:24:01 -0500

    generic api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from api import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^$', views.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
